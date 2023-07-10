3fb20a16b83d8a052866a0acb82ef2317e6e2dd2
Yogesh Palshikar <yogeshpalshikar1992@gmail.com>
2023-07-10 07:33:18 +0530

    removed static image

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -166,8 +166,7 @@ const CoffeeStore = (props) => {
                 <div className={styles.nameWrapper}>
                     <h1 className={styles.name}>{name}</h1>
                 </div>              
-                <Image src={imgUrl || 
-                  "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"} 
+                <Image src={imgUrl} 
                   width={600} 
                   height={360}
                   className={styles.storeImg} 
